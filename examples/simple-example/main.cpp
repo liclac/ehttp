e28341b08284b3d0b253d7ccc3a281e5f5739a34
uppfinnarn <uppfinnarn@gmail.com>
2014-06-02 02:22:19 +0200

    Example now prints responses to stdout

--- examples/simple-example/main.cpp
+++ examples/simple-example/main.cpp
@@ -3,6 +3,7 @@
 #include <ehttp/server.h>
 #include <ehttp/parser.h>
 #include <ehttp/request.h>
+#include <ehttp/response.h>
 
 int main(int argc, const char **argv)
 {
@@ -15,16 +16,39 @@ int main(int argc, const char **argv)
 		//std::cout << std::string(static_cast<char*>(data), size) << std::endl;
 		parser.parse_chunk(data, size);
 	};
-	srv.on_error = [&](asio::error_code error) {
+	srv.on_error = [](asio::error_code error) {
 		std::cerr << "Error: " << error.message() << std::endl;
 	};
 	
 	
 	
-	parser.on_request = [&](std::shared_ptr<ehttp::request> req) {
+	parser.on_request = [](std::shared_ptr<ehttp::request> req) {
 		std::cout << "Got a request for " << req->url << std::endl;
+		
+		auto res = std::make_shared<ehttp::response>(req);
+		res->on_end = [](std::shared_ptr<ehttp::response> res) {
+			std::vector<char> http = res->to_http();
+			std::cout << std::string(http.begin(), http.end()) << std::endl;
+		};
+		res->on_chunk = [](std::shared_ptr<ehttp::response> res, std::shared_ptr<ehttp::response::chunk> chunk) {
+			std::vector<char> http = chunk->to_http();
+			std::cout << std::string(http.begin(), http.end()) << std::endl;
+		};
+		
+		try
+		{
+			res->begin(200);
+			res->header("Content-Type", "text/plain");
+			res->write("Lorem ipsum dolor sit amet");
+			res->end();
+		}
+		catch(std::exception *e)
+		{
+			std::cerr << "EXCEPTION: " << e->what() << std::endl;
+			throw e;
+		}
 	};
-	parser.on_error = [&]() {
+	parser.on_error = []() {
 		std::cerr << "Parser Error" << std::endl;
 	};
 	
