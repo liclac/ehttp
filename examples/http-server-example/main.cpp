e27b88642ac5882ca92405bb58bd2db76c250ca8
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-02 01:55:29 +0200

    Commented examples

--- examples/http-server-example/main.cpp
+++ examples/http-server-example/main.cpp
@@ -5,17 +5,24 @@ using namespace ehttp;
 
 int main(int argc, char **argv)
 {
+	// Create a server
 	http_server srv;
+	
+	// Register a request handler
 	srv.on_request = [=](std::shared_ptr<server::connection>, std::shared_ptr<request> req, std::shared_ptr<response> res) {
 		res->begin()
 			->header("Content-Type", "text/plain")
 			->write("Lorem ipsum dolor sit amet")
 			->end();
 	};
+	
+	// Handle server errors (network issues, etc)
 	srv.on_error = [=](asio::error_code error) {
 		std::cout << "Error: " << error << std::endl;
 	};
 	
+	// Attempt to bind to (listen on) a port.
+	// Note that this may fail! See the documentation for more details.
 	asio::error_code error = srv.listen(8080);
 	if(!error)
 	{
@@ -25,6 +32,7 @@ int main(int argc, char **argv)
 	else
 	{
 		std::cout << "Couldn't listen: " << error.message() << std::endl;
+		return 1;
 	}
 	
 	return 0;
