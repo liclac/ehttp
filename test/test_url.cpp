e96390c6ae49d980965a1b1f3519662f3e479f0b
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-03 20:48:32 +0200

    Oops, I misunderstood the syntax here

--- test/test_url.cpp
+++ test/test_url.cpp
@@ -4,28 +4,28 @@
 #include <iostream>
 #include <ehttp/url.h>
 
-TEST_CASE("url/complete_url", "Complete URL")
+TEST_CASE("Complete URL")
 {
 	std::string str("http://example.com/path/to/something?q1=abc&q2=xyz#fragment");
 	ehttp::url url(str);
 	REQUIRE(url.str() == str);
 }
 
-TEST_CASE("url/no_suffixes", "URL with no query or fragment")
+TEST_CASE("URL with no query or fragment")
 {
 	std::string str("http://example.com/path/to/something");
 	ehttp::url url(str);
 	REQUIRE(url.str() == str);
 }
 
-TEST_CASE("url/absolute_path", "URL with only an absolute path")
+TEST_CASE("URL with only an absolute path")
 {
 	std::string str("/path/to/something");
 	ehttp::url url(str);
 	REQUIRE(url.str() == str);
 }
 
-TEST_CASE("url/relative_path", "URL with only an relative path")
+TEST_CASE("URL with only an relative path")
 {
 	std::string str("path/to/something");
 	ehttp::url url(str);
